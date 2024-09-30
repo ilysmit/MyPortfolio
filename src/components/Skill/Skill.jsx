d7b8c1fe6b3872b5dd94b9eebaff9e7539a618ea
smit <itsmit6879@gmail.com>
2024-09-30 20:14:57 +0530

    add new files in skill components

---
+++ src/components/Skill/Skill.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./skill.css";
+import { SKILLS } from "../../utils/data";
+import SkillCard from "./SkillCard/SkillCard";
+import SkillInfoCard from "./SkillInfoCard/SkillInfoCard";
+
+function Skill() {
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+
+  const handleSelactSkill = (data) => {
+    setSelectedSkill(data);
+  };
+
+  return (
+    <section className="skills-container">
+      <h5>Technical Proficiency</h5>
+
+      <div className="skills-content">
+        <div className="skills">
+          {SKILLS.map((item) => (
+            <SkillCard
+              key={item.title}
+              iconUrl={item.icon}
+              title={item.title}
+              isActive={selectedSkill.title===item.title}
+              onClick={()=>{
+                handleSelactSkill(item);
+              }}
+            />
+          ))}
+        </div>
+
+        <div className="skills-info">
+          <SkillInfoCard
+          heading={selectedSkill.title}
+          skills={selectedSkill.skills}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Skill;
---
+++ src/utils/data.js
@@ -0,0 +1,41 @@
+export const SKILLS = [
+    {
+        title:"Frontend",
+        icon:"./assets/images/front.png",
+        skills:[
+            {skill:"HTML5",percentage:"80%"},
+            {skill:"CSS3",percentage:"80%"},
+            {skill:"Javascript(ES6)",percentage:"70%"},
+            {skill:"React Js ",percentage:"80%"},
+            {skill:"Three Js ",percentage:"Learning"},
+
+        ],
+    },
+    {
+        title:"Backend",
+        icon:"./assets/images/back.png",
+        skills:[
+            {skill:"PHP",percentage:"50%"},
+            {skill:"MSQL",percentage:"50%"},
+        ],
+    },
+    {
+        title:"Tools",
+        icon:"./assets/images/tool.png",
+        skills:[
+            {skill:"Git & Github,Gitleb",percentage:"80%"},
+            {skill:"Postmane",percentage:"80%"},
+            {skill:"Figma , Adobe XD ",percentage:"80%"},
+            {skill:"Bootstrap , Tailwind Css ",percentage:"80%"},
+        ],
+    },
+    {
+        title:"Soft-Skills",
+        icon:"./assets/images/soft.png",
+        skills:[
+            {skill:"Problem-Solving",percentage:"80%"},
+            {skill:"Collaboration",percentage:"80%"},
+            {skill:"Creative Design",percentage:"80%"},
+        ],
+    },
+];
